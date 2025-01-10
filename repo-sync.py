2f846b8438b155772f560425d77862a9419161d3
Michal Čihař <michal@cihar.com>
2025-01-10 10:52:28 +0100

    feat: improve ruff configuration

--- repo-sync.py
+++ repo-sync.py
@@ -1,3 +1,8 @@
+"""
+Synchronizes shared content between Python Social Auth repositories.
+
+Execute: uv run repo-sync.py
+"""
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
@@ -6,10 +11,12 @@
 # ///
 
 from __future__ import annotations
+
 import subprocess
 from pathlib import Path
-from termcolor import colored
 from shutil import copyfile
+
+from termcolor import colored
 
 REPOSITORIES = (
     # Core has to be the first as it is used to update some of the files
@@ -30,11 +37,15 @@ Automated update of shared files from the social-core repository.
 
 
 def highlight(value: str) -> str:
+    """Highlight string for terminal output."""
     return colored(value, "cyan")
 
 
 class Repository:
-    def __init__(self, name: str, base: Path):
+    """Repository wrapper class to handle updates."""
+
+    def __init__(self, name: str, base: Path) -> None:
+        """Repository initiliazition."""
         self.name = name
         self.directory = REPOS / name
         self.base = base
@@ -42,17 +53,19 @@ class Repository:
     def run(
         self, args: list[str], *, cwd: Path | None = None, check: bool = True
     ) -> subprocess.CompletedProcess:
+        """Execute a process in repository."""
         if cwd is None:
             cwd = self.directory
         return subprocess.run(args, cwd=cwd, check=check)
 
     def checkout(self) -> None:
+        """Checkout or update working copy."""
         if self.directory.exists():
             print(f"Updating {highlight(self.name)}...")
             # Update
             self.run(["git", "reset", "--quiet", "--hard", "origin/HEAD"])
             self.run(["git", "remote", "prune", "origin"])
-            subprocess.run(["git", "pull", "--quiet"])
+            subprocess.run(["git", "pull", "--quiet"], check=False)
         else:
             print(f"Cloning {highlight(self.name)}...")
             # Initial checkout
@@ -63,12 +76,14 @@ class Repository:
             )
 
     def update_files(self) -> None:
+        """Update files from the base repository."""
         if self.base == self.directory:
             return
         for name in COPY_FROM_BASE:
             copyfile(self.base / name, self.directory / name)
 
     def commit(self) -> None:
+        """Commit and push pending changes."""
         self.run(["git", "add", "."])
         if self.run(["git", "diff", "--cached", "--exit-code"], check=False).returncode:
             print(f"Committing {highlight(self.name)}...")
@@ -77,6 +92,7 @@ class Repository:
 
 
 def main() -> None:
+    """Update all repositories."""
     # Base repository (social-core)
     base = REPOS / REPOSITORIES[0]
 
